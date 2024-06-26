cb3d226600113249ba6849131111d3a06436be65
KRizk-Intouch <96003776+KRizk-Intouch@users.noreply.github.com>
2024-06-26 22:19:00 +0300

    feat: route & request validation

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreOrderRequest;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreOrderRequest $request)
+    {
+
+    }
+}
